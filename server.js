3f434cb922473617417fa6857261d524e7737dee
Ismael <ismael@oppio.com.br>
2016-02-01 19:32:51 -0200

    Add /todos ans /todos/:id

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Ir para Curitiba',
+	completed: false
+},
+{
+	id: 2,
+	description: 'Estudar Laravel',
+	completed: false
+},
+{
+	id: 3,
+	description: 'Looks god',
+	completed: true
+}];
 
 app.get('/',function(req, res){
 	res.send('Todo API Root');
 });
+
+//GET /todos
+app.get('/todos',function(req,res){
+	res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id',function(req, res){
+	var id = parseInt(req.params.id, 10);
+	var todoObj;
+
+	todos.forEach(function(item){
+		if(item.id === id){
+			todoObj = item;
+		}
+	})
+
+	if(todoObj){
+		res.json(todoObj);	
+	}
+	
+	res.status(404).send();
+
+});
+
 
 app.listen(PORT,function(){
 	console.log('Express listening on port '+ PORT);
