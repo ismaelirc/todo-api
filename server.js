a732c691595692e69fe15aff1b6a39949a721fcd
Ismael <ismaelirc@gmail.com>
2016-02-24 09:50:02 -0300

    Adding user authentication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcryptjs = require('bcryptjs');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -155,7 +156,7 @@ app.put('/todos/:id', function(req, res) {
 
 
 //post para criar novos usuários
-app.post('/users',function(req, res){
+app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
@@ -165,6 +166,22 @@ app.post('/users',function(req, res){
 	}, function(e) {
 		res.status(400).json(e);
 	});
+});
+
+
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user){
+
+		res.json(user.toPublicJSON())
+
+	},function(e){
+		res.status(401).send();
+
+	});
+
+
 });
 
 db.sequelize.sync({
