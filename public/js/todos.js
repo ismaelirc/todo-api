10e9e2ebbdcd9a3bf973312a617f3c880c51028a
Ismael <ismaelirc@gmail.com>
2016-03-09 15:51:18 -0300

    Add logout feature, Validate form and date field to todo

--- public/js/todos.js
+++ public/js/todos.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+	//UI Components
+	
+    $( "#date").datepicker({
+    	dateFormat: "yy-mm-dd"
+    });
+
+     $("#date").mask("9999-99-99");
+  	
+    //Runs everytime when this page id load. Get All todos in base
 	$.ajax({
 		url: "/todos",
 		type: "GET",
@@ -21,14 +30,22 @@ $(document).ready(function() {
 		}
 	});
 
+	//Save new todo
 	$('#form-add-todo').on('submit',function(event){
 		event.preventDefault();
 
-		var desc = $('#description').val();
+		var desc = $.trim($('#description').val());
+		var date = $.trim($('#date').val());
 		var completed = false;
 
 		if($("#completed").is(':checked')){
 			completed = true;
+		}
+
+		if(!desc || !date){
+			alert('Please, all fields are required.');
+			$('#description').focus();
+			return false;
 		}
 
 		$.ajax({
@@ -37,7 +54,7 @@ $(document).ready(function() {
 			data: JSON.stringify({
 				description: desc,
 				completed: completed,
-				date: '2016-12-31'
+				date: date
 			}),
 			beforeSend:function(request){
 				request.setRequestHeader("Auth",localStorage.getItem('Auth'));
@@ -46,6 +63,9 @@ $(document).ready(function() {
 			dataType: "json",
 			success: function(data, textStatus, request) {
 				if (data) {
+					$('#description').val('');
+					$('#date').val('');
+
 					var jsonResult = [data];
 					var html = tableTodoBuild(jsonResult);
 					
@@ -58,7 +78,31 @@ $(document).ready(function() {
 				console.log(jqXHR.status)
 			}
 		});
+	});
 
+	//Logout Button Clicked
+	$("#logout").on('click',function(){
+
+		$.ajax({
+			url: "/users/login",
+			type: "DELETE",
+			beforeSend:function(request){
+				request.setRequestHeader("Auth",localStorage.getItem('Auth'));
+			},
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(data, textStatus, request) {
+				
+					
+					window.location.href = '/index.html';
+					
+				
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				//switch (jqXHR.status)
+				console.log(jqXHR.status)
+			}
+		});
 
 	});
 });
